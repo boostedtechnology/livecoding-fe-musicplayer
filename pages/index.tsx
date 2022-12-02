7b017dcedeeab4c32681729d5d5c5313aee1de6d
malokevi <malokevi@gmail.com>
2022-12-01 19:54:33 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 import { Layout, PlayerLayout, TrackList, TrackListItem } from "../components"
 import { usePlayerContext } from "../components/providers/PlayerContextProvider"
 
@@ -5,17 +7,23 @@ export default function Home() {
   const { trackList } = usePlayerContext()
 
   return (
-    <Layout>
-      <TrackList>
-        {trackList.map((props, i) => (
-          <TrackListItem
-            key={`${props.title}-${props.artist}`}
-            trackNumber={i + 1}
-            track={props}
-          />
-        ))}
-      </TrackList>
-      <PlayerLayout />
-    </Layout>
+    <>
+      <Head>
+        <title>Music Player</title>
+        <meta property="og:title" content="Music Player" key="title" />
+      </Head>
+      <Layout>
+        <TrackList>
+          {trackList.map((props, i) => (
+            <TrackListItem
+              key={`${props.title}-${props.artist}`}
+              trackNumber={i + 1}
+              track={props}
+            />
+          ))}
+        </TrackList>
+        <PlayerLayout />
+      </Layout>
+    </>
   )
 }
